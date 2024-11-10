505deaaa1168c9223587a0364befd79ed2ef22be
Chaya Stern <chayastern@triplewhale.com>
2024-11-11 01:53:43 +0200

    sku

--- src/CatalogItem.tsx
+++ src/CatalogItem.tsx
@@ -3,6 +3,7 @@ import { CatalogItem } from "./types";
 const CatalogItemCard: React.FC<{ item: CatalogItem; recommended?: boolean }> = ({ item, recommended }) => {
   return (
     <div className={`catalog-item ${item.isSoldOut ? 'sold-out' : ''} ${recommended ? 'recommended' : ''}`} key={item.sku}>
+      <span className="sku">#{item.sku}</span>
       <div className="img-container">
         <img src={item.imgUrl} alt={item.text} width="150" height="150" />
       </div>
